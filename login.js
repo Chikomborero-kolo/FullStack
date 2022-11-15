293829a0f6c95035e82864e53d864bae8fd3ea3f
Chikomborero-kolo <61728612+Chikomborero-kolo@users.noreply.github.com>
2022-11-15 10:38:33 +0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,14 @@
+ }
+},[])
+return(
+  <div>
+    <Header />
+      <h1> Login Page <h1/>
+      <div className = "col-sm-6 offset-sm-3">
+      <input type = "text" placeholder = "email" className = "form-control" />
+      <br/>
+      <button className = "btn"> Login </button>
+   </div>
+)
+}
+      
